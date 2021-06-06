865f5e19b3d5ee503cabbf9799ad64e0dec35ae8
jrphan <73537516+jrphan@users.noreply.github.com>
2021-06-06 18:48:07 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var $carousel = $('.sec3__inner-slider').flickity({
+    imagesLoaded: true,
+    percentPosition: false,
+    wrapAround: true,
+    autoPlay: 1500
+});
+  
+  var $imgs = $carousel.find('.item img');
+  // get transform property
+  var docStyle = document.documentElement.style;
+  var transformProp = typeof docStyle.transform == 'string' ?
+    'transform' : 'WebkitTransform';
+  // get Flickity instance
+  var flkty = $carousel.data('flickity');
+  
+  $carousel.on( 'scroll.flickity', function() {
+    flkty.slides.forEach( function( slide, i ) {
+      var img = $imgs[i];
+      var x = ( slide.target + flkty.x ) * -1/3;
+      img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+    });
+  });
+
+  const r = rolly({
+    view: document.querySelector('.app'),
+    native: true,
+    // other options
+  });
+  r.init();
