faf565efa1a7b037340dd4b5a520166026e3c1bb
jrphan <73537516+jrphan@users.noreply.github.com>
2021-06-06 21:22:29 +0700

    custom mouse

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const cursor = document.querySelector('.cursor');
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+})
+
 var $carousel = $('.sec3__inner-slider').flickity({
     imagesLoaded: true,
     percentPosition: false,
